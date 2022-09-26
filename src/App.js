02d0e3b72de3fd838951ab73e469e13bebb301cb
Vinipyh31 <52539401+Vinipyh31@users.noreply.github.com>
2022-09-26 18:58:33 +0500

    added prototype for more currencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { findAllByAltText } from '@testing-library/react';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
@@ -9,13 +8,13 @@ function App() {
   const [toCurrency, setToCurrency] = useState('USD')
   const [fromPrice, setFromPrice] = useState(0)
   const [toPrice, setToPrice] = useState(0)
-  
+  const [modalOpen, setModalOpen] = useState(false)
   const [rates, setRates] = useState({})
 
   useEffect(() => {
     fetch('https://cdn.cur.su/api/latest.json')
-    .then((res) => res.json())
-      .then((res) => { setRates(res.rates)})
+      .then((res) => res.json())
+      .then((res) => { setRates(res.rates) })
   }, [])
 
   useEffect(() => {
@@ -25,6 +24,7 @@ function App() {
   useEffect(() => {
     onChangeToPrice(toPrice)
   }, [toCurrency])
+
 
   const onChangeFromCurrency = (cur) => {
     setFromCurrency(cur)
@@ -38,19 +38,69 @@ function App() {
     const price = value / rates[fromCurrency];
     const result = price * rates[toCurrency];
     setFromPrice(value)
-    setToPrice(result.toFixed(3))
+    setToPrice(result.toFixed(2))
   }
 
   const onChangeToPrice = (value) => {
     const price = value / rates[toCurrency];
     const result = price * rates[fromCurrency];
     setToPrice(value)
-    setFromPrice(result.toFixed(3))
+    setFromPrice(result.toFixed(2))
   }
+
+  const switchModalOpen = () => {
+    setModalOpen(!modalOpen);
+  }
+
   return (
-    <div className="App">
-      <Block value={+fromPrice} currency={fromCurrency} onChangeCurrency={onChangeFromCurrency} onChangeValue={onChangeFromPrice} />
-      <Block value={+toPrice} currency={toCurrency} onChangeCurrency={onChangeToCurrency} onChangeValue={onChangeToPrice}/>
+    <div>
+      <div className="App">
+        <Block
+          value={+fromPrice}
+          currency={fromCurrency}
+          onChangeCurrency={onChangeFromCurrency}
+          onChangeValue={onChangeFromPrice}
+          switchModalOpen={switchModalOpen}
+        />
+        <Block
+          value={+toPrice}
+          currency={toCurrency}
+          onChangeCurrency={onChangeToCurrency}
+          onChangeValue={onChangeToPrice}
+          switchModalOpen={switchModalOpen}
+        />
+
+      </div>
+      <div
+        className={`modal${modalOpen ? " show" : ""}`}
+      >
+        <svg
+          height="30"
+          viewBox="0 0 200 200"
+          width="30"
+          onClick={switchModalOpen}
+          cursor="pointer"
+          style={{
+            textAlign: "right",
+          }}
+        >
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        <ul>
+          {modalOpen && Object.keys(rates).map((rate, index) => {
+            const curName = rate;
+            return (
+              <li
+                onClick={() => { 
+                  onChangeFromCurrency(curName);
+                  switchModalOpen();
+                }}
+              >{rate}</li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
