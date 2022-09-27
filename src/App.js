46445d4cd2d52fc9ee4eb9644380e8c327675753
Vinipyh31 <52539401+Vinipyh31@users.noreply.github.com>
2022-09-27 16:29:49 +0500

    make Modal component and style him

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Block } from './Block';
+import { Block } from './components/Block';
+import Modal from './components/Modal';
 import './index.scss';
+
+const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
 
 function App() {
 
@@ -10,11 +13,24 @@ function App() {
   const [toPrice, setToPrice] = useState(0)
   const [modalOpen, setModalOpen] = useState(false)
   const [rates, setRates] = useState({})
+  const [modalType, setModalType] = useState()
 
   useEffect(() => {
     fetch('https://cdn.cur.su/api/latest.json')
       .then((res) => res.json())
-      .then((res) => { setRates(res.rates) })
+      .then((res) => { setRates(res.rates) });
+
+    const keyDownHandler = event => {
+      if (event.key === 'Escape') {
+        event.preventDefault();
+        setModalOpen(false)
+      }
+    };
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+
   }, [])
 
   useEffect(() => {
@@ -24,7 +40,6 @@ function App() {
   useEffect(() => {
     onChangeToPrice(toPrice)
   }, [toCurrency])
-
 
   const onChangeFromCurrency = (cur) => {
     setFromCurrency(cur)
@@ -52,6 +67,8 @@ function App() {
     setModalOpen(!modalOpen);
   }
 
+
+
   return (
     <div>
       <div className="App">
@@ -61,6 +78,8 @@ function App() {
           onChangeCurrency={onChangeFromCurrency}
           onChangeValue={onChangeFromPrice}
           switchModalOpen={switchModalOpen}
+          defaultCurrencies={defaultCurrencies}
+          setModalType={() => { setModalType("from") }}
         />
         <Block
           value={+toPrice}
@@ -68,40 +87,18 @@ function App() {
           onChangeCurrency={onChangeToCurrency}
           onChangeValue={onChangeToPrice}
           switchModalOpen={switchModalOpen}
+          defaultCurrencies={defaultCurrencies}
+          setModalType={() => { setModalType("to") }}
         />
 
       </div>
-      <div
-        className={`modal${modalOpen ? " show" : ""}`}
-      >
-        <svg
-          height="30"
-          viewBox="0 0 200 200"
-          width="30"
-          onClick={switchModalOpen}
-          cursor="pointer"
-          style={{
-            textAlign: "right",
-          }}
-        >
-          <title />
-          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-        </svg>
-        <ul
-          className='currency-list'>
-          {modalOpen && Object.keys(rates).map((rate, index) => {
-            const curName = rate;
-            return (
-              <li
-                onClick={() => {
-                  onChangeFromCurrency(curName);
-                  switchModalOpen();
-                }}
-              >{rate}</li>
-            )
-          })}
-        </ul>
-      </div>
+      <Modal
+        modalOpen={modalOpen}
+        switchModalOpen={switchModalOpen}
+        rates={rates}
+        currency={modalType === "from" ? fromCurrency : toCurrency}
+        onChangeCurrency={modalType === "from" ? onChangeFromCurrency : onChangeToCurrency}
+      />
     </div>
   );
 }
--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
 
-export const Block = ({ value, currency, onChangeValue, onChangeCurrency, switchModalOpen }) => (
+export const Block = ({ value, currency, onChangeValue, onChangeCurrency, switchModalOpen, defaultCurrencies, setModalType }) => (
   <div className="block">
     <ul className="currencies">
       {defaultCurrencies.map((cur) => (
@@ -15,7 +14,10 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency, switch
       ))}
       <li
         className={defaultCurrencies.includes(currency) ? '' : 'active'}
-        onClick={switchModalOpen}>
+        onClick={() => {
+          switchModalOpen();
+          setModalType();
+        }}>
         <svg height="50px" viewBox="0 0 50 50" width="50px">
           <rect fill="none" height="50" width="50" />
           <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
